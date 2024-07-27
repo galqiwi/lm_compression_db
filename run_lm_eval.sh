50aa2c936dfe3c9184e15124df062b26b125d52b
Vladimir Malinovskii <galqiwi@gmail.com>
2024-07-27 09:49:59 +0300

    upd

--- run_lm_eval.sh
+++ run_lm_eval.sh
@@ -5,17 +5,17 @@ set -eu
 echo "$#"
 
 PUBLISHER="$1"
-echo "$PUBLISHER"
+echo PUBLISHER="$PUBLISHER"
 MODEL="$2"
-echo "$MODEL"
+echo MODEL="$MODEL"
 
-mkdir -p "./$PUBLISHER/$MODEL"
+mkdir -p "./models/$PUBLISHER/$MODEL"
 
 run_one_task() {
   TASK="$1"
   NUM_FEWSHOT="$2"
   echo TASK="$TASK"
-  python3 run_lm_eval.py --model_name "$PUBLISHER/$MODEL" --save_json "./$PUBLISHER/$MODEL/""$TASK"_"$NUM_FEWSHOT".json --task "$TASK"
+  python3 run_lm_eval.py --model_name "$PUBLISHER/$MODEL" --save_json "./models/$PUBLISHER/$MODEL/""$TASK"_"$NUM_FEWSHOT".json --task "$TASK"
 }
 
 run_one_task mmlu_continuation_high_school_government_and_politics 1
